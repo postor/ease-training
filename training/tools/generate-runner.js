9b586dd33dbeb510622785603467052904d3dde2
josh <postor@gmail.com>
2019-12-09 11:23:58 +0800

    generate-runner.js

--- training/tools/generate-runner.js
+++ training/tools/generate-runner.js
@@ -20,32 +20,36 @@ const Dataset = getModel('dataset')
 
   ;
 (async () => {
-  let todoList = await getJob(WORKING_STATE_NOT_STARTED)
-  while (!todoList.length) {
-    console.log(`No job to do, wait for ${IDLE_WAITING} miliseconds ...`)
-    await waitMiliseconds(IDLE_WAITING)
-    todoList = await getJob(1)
+  try {
+    let todoList = await getJob(WORKING_STATE_NOT_STARTED)
+    while (!todoList.length) {
+      console.log(`No job to do, wait for ${IDLE_WAITING} miliseconds ...`)
+      await waitMiliseconds(IDLE_WAITING)
+      todoList = await getJob(1)
+    }
+    const train = todoList[0]
+    console.log(`Job found, ${JSON.stringify(train)}`)
+    train.working = WORKING_STATE_WORKING
+    await Train.update({ data: train, params: { id: train.id } })
+    console.log(`Job updated, ${JSON.stringify(train)}`)
+    const model = (await Model.readOne({ params: { id: train.model_id } })).data
+    const dataset = (await Dataset.readOne({ params: { id: train.dataset_id } })).data
+
+    const params = [
+      '--rm',
+      '--gpus all',
+      `--env TRAIN_ID=${train.id}`,
+      `--env RESTFUL_ENDPOINT=${process.env.RESTFUL_ENDPOINT}`,
+      `-v ${process.env.SHARED_FILES}:/shared-files`,
+      `-v ${process.env.SHARED_FILES + '/datasets/' + dataset.name}:/dataset.zip`,
+    ]
+    const cmd = `${model.docker_cmd} --save-prefix=/shared-files/params/${dataset.name}/${model.name}`
+    fs.writeFileSync(join(__dirname, 'tmp.sh'),
+      `set -x && docker run ${params.join(' ')} postor/ease-training ${cmd}`)
+  } catch (e) {
+    console.log(e)
+    process.exit(2)
   }
-  const train = todoList[0]
-  console.log(`Job found, ${JSON.stringify(train)}`)
-  train.working = WORKING_STATE_WORKING
-  await Train.update({ data: train, params: { id: train.id } })
-  console.log(`Job updated, ${JSON.stringify(train)}`)
-  const model =  (await Model.readOne({ params: { id: train.model_id } })).data
-  const dataset = (await Dataset.readOne({ params: { id: train.dataset_id } })).data
-
-  const params = [
-    '--rm',
-    '--gpus all',
-    `--env TRAIN_ID=${train.id}`,
-    `--env RESTFUL_ENDPOINT=${process.env.RESTFUL_ENDPOINT}`,
-    `-v ${process.env.SHARED_FILES}:/shared-files`,
-    `-v ${process.env.SHARED_FILES + '/datasets/' + dataset.name}:/dataset.zip`,
-  ]
-  const cmd = `${model.docker_cmd} --save-prefix=/shared-files/params/${dataset.name}/${model.name}`
-  fs.writeFileSync(join(__dirname, 'tmp.sh'),
-    `set -x && docker run ${params.join(' ')} postor/ease-training ${cmd}`)
-
 })()
 
 async function getJob(workingState) {
