6e90a1bca21ab6bbb08e50a03e9c49f8158c3a3e
josh <postor@gmail.com>
2019-12-10 18:37:36 +0800

    basic

--- mysql/schema.sql
+++ mysql/schema.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 2019-12-10 10:44:16
+-- Generation Time: 2019-12-10 11:35:32
 -- 服务器版本： 10.1.31-MariaDB
 -- PHP Version: 7.2.4
 
@@ -69,16 +69,16 @@ CREATE TABLE `model` (
 --
 
 INSERT INTO `model` (`id`, `name`, `docker_cmd`) VALUES
-(1, 'yolo3_darknet53_custom', 'train_yolo3.py --batch-size=2 --gpus=1,2 --lr=0.0001 --epochs=500 --network=darknet53'),
-(2, 'yolo3_mobilenet0.25_custom', 'train_yolo3.py --batch-size=2 --gpus=1,2 --lr=0.0001 --epochs=500 --network=mobilenet0.25'),
-(3, 'yolo3_mobilenet1.0_custom', 'train_yolo3.py --batch-size=2 --gpus=1,2 --lr=0.0001 --epochs=500 --network=mobilenet1.0'),
-(4, 'faster_rcnn_resnet101_v1d_custom', 'train_faster_rcnn.py --batch-size=2 --gpus=1,2 --lr=0.0001 --epochs=500 --network=resnet101_v1d'),
-(5, 'faster_rcnn_resnet50_v1b_custom', 'train_faster_rcnn.py --batch-size=2 --gpus=1,2 --lr=0.0001 --epochs=500 --network=resnet50_v1b'),
-(6, 'ssd_300_mobilenet0.25_custom', 'train_ssd.py --batch-size=2 --gpus=1,2 --lr=0.0001 --epochs=500 --network=mobilenet0.25  --data-shape=300'),
-(7, 'ssd_300_vgg16_atrous_custom', 'train_ssd.py --batch-size=2 --gpus=1,2 --lr=0.0001 --epochs=500 --network=vgg16_atrous  --data-shape=300'),
-(8, 'ssd_512_mobilenet1.0_custom', 'train_ssd.py --batch-size=2 --gpus=1,2 --lr=0.0001 --epochs=500 --network=mobilenet1.0  --data-shape=512'),
-(9, 'ssd_512_resnet50_v1_custom', 'train_ssd.py --batch-size=2 --gpus=1,2 --lr=0.0001 --epochs=500 --network=resnet50_v1  --data-shape=512'),
-(10, 'ssd_512_vgg16_atrous_custom', 'train_ssd.py --batch-size=2 --gpus=1,2 --lr=0.0001 --epochs=500 --network=vgg16_atrous  --data-shape=512');
+(1, 'yolo3_darknet53_custom', 'train_yolo3.py --gpus=1,2 --network=darknet53'),
+(2, 'yolo3_mobilenet0.25_custom', 'train_yolo3.py --gpus=1,2 --network=mobilenet0.25'),
+(3, 'yolo3_mobilenet1.0_custom', 'train_yolo3.py --gpus=1,2 --network=mobilenet1.0'),
+(4, 'faster_rcnn_resnet101_v1d_custom', 'train_faster_rcnn.py --gpus=1,2 --network=resnet101_v1d'),
+(5, 'faster_rcnn_resnet50_v1b_custom', 'train_faster_rcnn.py --gpus=1,2 --network=resnet50_v1b'),
+(6, 'ssd_300_mobilenet0.25_custom', 'train_ssd.py --gpus=1,2 --network=mobilenet0.25  --data-shape=300'),
+(7, 'ssd_300_vgg16_atrous_custom', 'train_ssd.py --gpus=1,2 --network=vgg16_atrous  --data-shape=300'),
+(8, 'ssd_512_mobilenet1.0_custom', 'train_ssd.py --gpus=1,2 --network=mobilenet1.0  --data-shape=512'),
+(9, 'ssd_512_resnet50_v1_custom', 'train_ssd.py --gpus=1,2 --network=resnet50_v1  --data-shape=512'),
+(10, 'ssd_512_vgg16_atrous_custom', 'train_ssd.py --gpus=1,2 --network=vgg16_atrous  --data-shape=512');
 
 -- --------------------------------------------------------
 
